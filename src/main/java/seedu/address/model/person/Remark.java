22d9464c7a93d6e9a7036f74dcb5d65be3421721
Marcus Tan <marcustan26@hotmail.com>
2020-09-24 23:42:13 +0800

    add remark command

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,44 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Person's phone number in the address book.
+ * Guarantees: immutable
+ */
+public class Remark {
+    public final String value;
+    public static final String MESSAGE_CONSTRAINTS =
+            "Remarks cannot be empty";
+    public static final String EMPTY_REMARK = "";
+    
+    
+    /**
+     * Constructs a {@code Remark}.
+     *
+     * @param value A valid remark.
+     */
+    public Remark(String value) {
+        requireNonNull(value);
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Remark // instanceof handles nulls
+                && value.equals(((Remark) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
