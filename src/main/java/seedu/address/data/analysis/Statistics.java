5968266ebe58bdeae55210ac8ae87a10db2f530c
Marcus Tan <marcustan26@hotmail.com>
2020-10-24 13:23:54 +0800

    start implementation of statistics class:

---
+++ src/main/java/seedu/address/data/analysis/Statistics.java
@@ -0,0 +1,26 @@
+package seedu.address.data.analysis;
+
+import seedu.address.model.tag.Tag;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+
+/**
+ * This class handles all computation related to time spent on tasks and lessons.
+ */
+public class Statistics {
+    /**
+     * Computes total duration of time spent on all tasks and lessons for the specified time period.
+     */
+    public static Integer tabulateTotalDuration(LocalDate startDate, LocalDate endDate) {
+        return null;
+    }
+    /**
+     * Computes total duration of time spent on all tasks and lessons under each module tag for the specified
+     * time period.
+     */    
+    public static HashMap<Tag, Integer> tabulateTimeSpentOnEachModule(LocalDate startDate, LocalDate endDate) {
+        return null;
+    }
+    public static 
+}
