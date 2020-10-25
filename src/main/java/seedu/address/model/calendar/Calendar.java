d7bb9faab50659ba2f29a307f14538e4bb319ca6
GabriellaTeh <e0425488@u.nus.edu>
2020-10-25 22:01:23 +0800

    solve checkstyle errors

--- src/main/java/seedu/address/model/calendar/Calendar.java
+++ src/main/java/seedu/address/model/calendar/Calendar.java
@@ -7,7 +7,6 @@ import java.util.Iterator;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-
 import seedu.address.model.task.Task;
 import seedu.address.model.task.exceptions.DuplicateTaskException;
 import seedu.address.model.task.exceptions.TaskNotFoundException;
